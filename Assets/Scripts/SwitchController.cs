0d8b30e20cf21864cd12c11e3161abcede3c2e4d
senoajisr <senoajisr@student.telkomuniversity.ac.id>
2024-02-28 06:53:57 +0700

    ch3.4

--- Assets/Scripts/SwitchController.cs
+++ Assets/Scripts/SwitchController.cs
@@ -4,20 +4,85 @@ using UnityEngine;
 
 public class SwitchController : MonoBehaviour
 {
-    public Collider ball;
-    public Material onMaterial;
-    public Material offMaterial;
-    public Renderer switchRenderer;
+  private enum SwitchState
+  {
+    Off,
+    On,
+    Blink
+  }
 
-    private bool isOn = false;
+  public Collider bola;
+  public Material offMaterial;
+  public Material onMaterial;
 
-    private void OnTriggerEnter(Collider other) {
-        isOn = !isOn;
+  private SwitchState state;
+  private Renderer renderer;
 
-        if (isOn) {
-            switchRenderer.material = onMaterial;
-        } else {
-            switchRenderer.material = offMaterial;
-        }
+  private void Start()
+  {
+    renderer = GetComponent<Renderer>();
+
+    Set(false);
+
+    StartCoroutine(BlinkTimerStart(5));
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other == bola)
+    {
+      Toggle();
     }
+  }
+
+  private void Set(bool active)
+  {
+    if (active == true)
+    {
+      state = SwitchState.On;
+      renderer.material = onMaterial;
+      StopAllCoroutines();
+    }
+    else
+    {
+      state = SwitchState.Off;
+      renderer.material = offMaterial;
+      StartCoroutine(BlinkTimerStart(5));
+    }
+  }
+
+  private void Toggle()
+  {
+    if (state == SwitchState.On)
+    {
+      Set(false);
+    }
+    else
+    {
+      Set(true);
+    }
+  }
+
+  private IEnumerator Blink(int times)
+  {
+    state = SwitchState.Blink;
+
+    for (int i = 0; i < times; i++)
+    {
+      renderer.material = onMaterial;
+      yield return new WaitForSeconds(0.5f);
+      renderer.material = offMaterial;
+      yield return new WaitForSeconds(0.5f);
+    }
+
+    state = SwitchState.Off;
+
+    StartCoroutine(BlinkTimerStart(5));
+  }
+
+  private IEnumerator BlinkTimerStart(float time)
+  {
+    yield return new WaitForSeconds(time);
+    StartCoroutine(Blink(2));
+  }
 }
