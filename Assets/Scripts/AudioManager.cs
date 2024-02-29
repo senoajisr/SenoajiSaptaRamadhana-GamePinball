16ffe53387819a27c14375c47cfadb59af472e36
senoajisr <senoajisr@student.telkomuniversity.ac.id>
2024-02-29 07:00:41 +0700

    ch5.1

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource bgmAudioSource;
+
+    private void Start()
+    {
+        PlayBGM();
+    }
+
+    public void PlayBGM()
+    {
+        bgmAudioSource.Play();
+    }
+
+    public void PlaySFX() { }
+}
